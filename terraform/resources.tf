ddfe096fdd900afb9156f83efe57543a4bc49768
Olga Kuksa <89924674+wawaqa@users.noreply.github.com>
2024-09-29 21:09:27 +0200

    feat: add terraform code

---
+++ terraform/resources.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "tf-example" {
+  bucket = var.tf_created_s3_bucket
+}
+
+resource "aws_iam_role" "GithubActionsRole" {
+  name               = "GithubActionsRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_username}/${var.repo_name}:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach_github_actions_policy" {
+  for_each   = toset(var.gh_iam_policies)
+  role       = aws_iam_role.GithubActionsRole.name
+  policy_arn = each.value
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,40 @@
+variable "tf_created_s3_bucket" {
+  description = "Name of the S3 bucket for storing the Terraform state"
+  type        = string
+  default     = "tf-created-bucket-someqa" 
+}
+
+variable "aws_region" {
+  description = "The AWS region where the S3 bucket is located"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "github_username" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "repo_name" {
+  description = "GitHub repository name"
+  type        = string
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID"
+  type        = string
+}
+
+variable "gh_iam_policies" {
+  description = "The List of Required IAM Policies"
+  type        = list(string)
+  default = [
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+  ]
+}
